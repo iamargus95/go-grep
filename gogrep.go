c9c3b5e762ef8b36a83b1bab50bc7c919852638d
iamargus95 <kamathsuraj95@gmail.com>
2021-08-31 12:07:42 +0530

    Switched to bufio for ReadFile function

--- gogrep.go
+++ gogrep.go
@@ -1,23 +1,39 @@
 package gogrep
 
 import (
-	"io/ioutil"
+	"bufio"
+	"log"
+	"os"
 	"strings"
 )
 
-func ReadFile(filepath string) ([]byte, error) {
-	data, err := ioutil.ReadFile(filepath)
-	return data, err
+func ReadFile(filepath string) ([]string, error) {
+	file, err := os.Open(filepath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var txtlines []string
+
+	for scanner.Scan() {
+		txtlines = append(txtlines, scanner.Text())
+	}
+
+	file.Close()
+
+	return txtlines, err
 }
 
-func Grep(fileContents []byte, pattern string) []string {
-	splitFile := strings.SplitAfter(string(fileContents), ".")
+func Grep(fileContents []string, pattern string) []string {
 	var outputString []string
-	for index, line := range splitFile {
+	for index, line := range fileContents {
 		if !strings.Contains(line, pattern) {
 
 		} else {
-			outputString = append(outputString, (splitFile[index])+"\n")
+			outputString = append(outputString, (fileContents[index]))
 		}
 	}
 	return outputString
--- gogrep_test.go
+++ gogrep_test.go
@@ -8,30 +8,30 @@ import (
 func TestGrep(t *testing.T) {
 	var nil1 []string
 	var testCases = []struct {
-		Files    []byte
+		Files    []string
 		Pattern  string
 		Expected []string
 	}{
 		{
-			Files:    []byte("This is One2N Consulting."),
+			Files:    []string{"This is One2N Consulting."},
 			Pattern:  "Software",
 			Expected: nil1,
 		},
 		{
-			Files:    []byte("This is One2N Consulting."),
+			Files:    []string{"This is One2N Consulting."},
 			Pattern:  "One2N",
-			Expected: []string{"This is One2N Consulting.\n"},
+			Expected: []string{"This is One2N Consulting."},
 		},
 		{
-			Files:    []byte("This is One2N Consulting. We employ Backend Engineers and Software Engineers."),
+			Files:    []string{"This is One2N Consulting.", "Backend Engineers and Software Engineers are employed."},
 			Pattern:  "Engineers",
-			Expected: []string{" We employ Backend Engineers and Software Engineers.\n"},
+			Expected: []string{"Backend Engineers and Software Engineers are employed."},
 		},
-		// {
-		// 	Files:    []byte("This is One2N Consulting.One2N employs Backend Engineers and Software Engineers."),
-		// 	Pattern:  "One2N",
-		// 	Expected: []string{"This is One2N Consulting.\n One2N employs Backend Engineers and Software Engineers.\n"},
-		// }, -------------------> Need for formatting arises.
+		{
+			Files:    []string{"This is One2N Consulting.", "One2N employs Backend Engineers and Software Engineers."},
+			Pattern:  "One2N",
+			Expected: []string{"This is One2N Consulting.", "One2N employs Backend Engineers and Software Engineers."},
+		},
 	}
 
 	for _, tc := range testCases {
