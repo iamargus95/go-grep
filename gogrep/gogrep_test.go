a97ddc0b9f373cdd38822e4408632943918fd849
iamargus95 <kamathsuraj95@gmail.com>
2021-09-01 13:30:17 +0530

    Added Tests for GrepCaseSensitive

--- gogrep/gogrep_test.go
+++ gogrep/gogrep_test.go
@@ -7,7 +7,7 @@ import (
 
 var nil1 []string
 
-var testCases = []struct {
+var testGrepNoFlag = []struct {
 	inputText []string
 	pattern   string
 	output    []string
@@ -36,8 +36,44 @@ var testCases = []struct {
 
 func TestGrep(t *testing.T) {
 
-	for _, tc := range testCases {
+	for _, tc := range testGrepNoFlag {
 		actual := Grep(tc.inputText, tc.pattern)
+		if !reflect.DeepEqual(actual, tc.output) {
+			t.Errorf("Error Found.")
+		}
+	}
+}
+
+var testGrepCaseSensitive = []struct {
+	inputText []string
+	pattern   string
+	output    []string
+}{
+	{
+		inputText: []string{"This is One2N Consulting."},
+		pattern:   "Software",
+		output:    nil1,
+	},
+	{
+		inputText: []string{"This is One2N Consulting."},
+		pattern:   "one2N",
+		output:    []string{"This is One2N Consulting."},
+	},
+	{
+		inputText: []string{"This is One2N Consulting.", "Backend Engineers and Software Engineers are employed."},
+		pattern:   "engineers",
+		output:    []string{"Backend Engineers and Software Engineers are employed."},
+	},
+	{
+		inputText: []string{"This is One2N Consulting.", "One2N employs Backend Engineers and Software Engineers."},
+		pattern:   "One2n",
+		output:    []string{"This is One2N Consulting.", "One2N employs Backend Engineers and Software Engineers."},
+	},
+}
+
+func TestGrepCaseSensitive(t *testing.T) {
+	for _, tc := range testGrepCaseSensitive {
+		actual := GrepCaseInsensitive(tc.inputText, tc.pattern)
 		if !reflect.DeepEqual(actual, tc.output) {
 			t.Errorf("Error Found.")
 		}
