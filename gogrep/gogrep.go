d196ee85e2d18c304f65f963c7c946eed3b940d5
iamargus95 <kamathsuraj95@gmail.com>
2021-09-02 11:04:17 +0530

    Improved test for before flag

--- gogrep/gogrep.go
+++ gogrep/gogrep.go
@@ -5,36 +5,6 @@ import (
 	"strings"
 )
 
-func GrepCaseInsensitive(fileContents []string, pattern string) []string {
-	var outputString []string
-	for index, line := range fileContents {
-		lineLower := strings.ToLower(line)
-		patternLower := strings.ToLower(pattern)
-		if strings.Contains(lineLower, patternLower) {
-			outputString = append(outputString, (fileContents[index]))
-			outputString = append(outputString, "--")
-		}
-	}
-	return outputString
-}
-
-func GrepCount(fileContents []string, pattern string) []string {
-	var outputString []string
-	var countOutput int
-	var outputSlice []string
-
-	for _, line := range fileContents {
-		if strings.Contains(line, pattern) {
-			countOutput += strings.Count(line, pattern)
-		}
-		outputStr := strconv.Itoa(countOutput)
-		outputString = append(outputString, outputStr)
-	}
-
-	outputSlice = append(outputSlice, outputString[(len(outputString)-1)])
-	return outputSlice
-}
-
 func Grep(fileContents []string, pattern string) []string {
 	var outputString []string
 	for index, line := range fileContents {
@@ -84,4 +54,34 @@ func GrepBefore(before int, fileContents []string, pattern string) []string {
 		}
 	}
 	return outputString
+}
+
+func GrepCaseInsensitive(fileContents []string, pattern string) []string {
+	var outputString []string
+	for index, line := range fileContents {
+		lineLower := strings.ToLower(line)
+		patternLower := strings.ToLower(pattern)
+		if strings.Contains(lineLower, patternLower) {
+			outputString = append(outputString, (fileContents[index]))
+			outputString = append(outputString, "--")
+		}
+	}
+	return outputString
+}
+
+func GrepCount(fileContents []string, pattern string) []string {
+	var outputString []string
+	var countOutput int
+	var outputSlice []string
+
+	for _, line := range fileContents {
+		if strings.Contains(line, pattern) {
+			countOutput += strings.Count(line, pattern)
+		}
+		outputStr := strconv.Itoa(countOutput)
+		outputString = append(outputString, outputStr)
+	}
+
+	outputSlice = append(outputSlice, outputString[(len(outputString)-1)])
+	return outputSlice
 }
